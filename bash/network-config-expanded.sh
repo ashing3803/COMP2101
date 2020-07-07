8baa914fc929ce35364e5fe37bfe2b69f0c6da38
ashing3803 <66534799+ashing3803@users.noreply.github.com>
2020-07-07 19:28:28 -0400

    lab 4

--- bash/network-config-expanded.sh
+++ bash/network-config-expanded.sh
@@ -61,40 +61,27 @@ EOF
 # Per-interface report
 #####
 # define the interface being summarized
-#interface="eno1"
+interface="eno1"
 
 # Find an address and hostname for the interface being summarized
 # we are assuming there is only one IPV4 address assigned to this interface
+ipv4_address=$(ip a s $interface|awk -F '[/ ]+' '/inet /{print $3}')
+ipv4_hostname=$(getent hosts $ipv4_address | awk '{print $2}')
 
 # Identify the network number for this interface and its name if it has one
-intlist=$(lshw -class network |  awk '/logical name:/{print $3}')
-declare -a interface=($intlist)
-len=${#interface[*]}
-i=0
-#Making a loop for processing each work in the array
-while [ $i -lt $len ]
-do
-    if [[ ${interface[$i]} = lo* ]] ;
-		then continue ;
-	fi
-	#Implimanting  the conditions to skip loopback interface
-      ipv4_address=$(ip a s ${interface[$i]} | awk -F '[/ ]+' '/inet /{print $3}')
-      ipv4_hostname=$(getent hosts $ipv4_address | awk '{print $2}')
-      network_address=$(ip route list dev ${interface[$i]} scope link|cut -d ' ' -f 1)
-      network_number=$(cut -d / -f 1 <<<"$network_address")
-      network_name=$(getent networks $network_number|awk '{print $1}')
-	  #Normal lines for putting command's output to the variable
-  echo Interface ${interface[$i]}:
-  echo ===============
-  echo Address         : $ipv4_address
-  echo Name            : $ipv4_hostname
-  echo Network Address : $network_address
-  echo Network Name    : $network_name
-  #echo for  printing everything.
+network_address=$(ip route list dev $interface scope link|cut -d ' ' -f 1)
+network_number=$(cut -d / -f 1 <<<"$network_address")
+network_name=$(getent networks $network_number|awk '{print $1}')
 
-  i=$((i+1))
-done
-#ending a loop
+cat <<EOF
+Interface $interface:
+===============
+Address         : $ipv4_address
+Name            : $ipv4_hostname
+Network Address : $network_address
+Network Name    : $network_name
+
+EOF
 #####
 # End of per-interface report
 #####
