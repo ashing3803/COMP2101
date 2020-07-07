f61460e8d1694f40b2b4539368c931cebe17ec2c
ashing3803 <66534799+ashing3803@users.noreply.github.com>
2020-07-07 19:28:12 -0400

    lab 3

--- bash/login-welcome.sh
+++ bash/login-welcome.sh
@@ -17,33 +17,15 @@
 ###############
 # Variables   #
 ###############
-userc="$USER" #By using the variables of environment to obtain username dynamically
-hostname="$(hostname)" #Using hostname to get it dynamicalyy
-day=$(date +%A) #By applying day we will be getting what day is 
-currenttime=$(date +%I:%M\ %p)
-  # by date command will get date
+title="Overlord"
+myname="dennis"
+hostname="myhostname"
+
 ###############
 # Main        #
 ###############
-if [ "$day" = Monday ]; #  Using if command to check day name and  addition of custom line to print message  by day name 
-then
-csay="Welcome to planet $hostname, nerd $userc!"
-elif [ "$day" = Tuesday ];
-then
-csay="Welcome to planet $hostname, system engineer $userc!"
-elif [ "$day" = Wednesday ];
-then
-csay="Welcome to planet $hostname, swift developer $userc!"
-elif [ "$day" = Thursday ];
-then
-csay="Welcome to planet $hostname, tech guardian $userc!"
-elif [ "$day" = Friday ];
-then
-csay="Welcome to planet $hostname, nutritionist $userc!"
-elif [ "$day" = Saturday ];
-then
-csay="Welcome to planet $hostname, Android developer $userc!"
-else
-csay="Welcome to planet $hostname, extrovert $userc!"
-fi
-cowsay "$csay It is $currenttime on $day ."
+cat <<EOF
+
+Welcome to planet $hostname, "$title $myname!"
+
+EOF
--- bash/passwordguesser.sh
+++ bash/passwordguesser.sh
@@ -9,44 +9,7 @@
 # TASK 3: Improve it by giving them 5 tries to get it right before failing
 #           *** Do not use the exit command, and do not use a loop (e.g. while or for)
 
-login="You found out!!!" 
-nologin="Nop, It's Incorrect."
-referenceString="Password" # making of  a variable to store password
-read -s -p "Insert secret character:" myString #get  userinput  for password and saving into $myString
-echo
-if [ $myString = $referenceString ]; then #if-else condition 5 times to check input is valid or not by comparing $referenceString.
-  echo "$login"
-  echo
-else
-    echo "$nologin"
-    read -s -p "Insert secret character:" myString
-  if [ $myString = $referenceString ]; then
-    echo "$login"
-    echo
-  else
-      echo
-      echo "$nologin"
-      read -s -p "Insert secret character:" myString
-    if [ $myString = $referenceString ]; then
-      echo "$login"
-      echo
-    else
-      echo
-      echo "$nologin"
-read -s -p "Insert secret character:" myString
-if [ $myString = $referenceString ]; then
-  echo "$login"
-  echo
-else
-    echo "$nologin"
-    read -s -p "Insert secret character:" myString
-	if [ $myString = $referenceString ]; then
-  echo "$login"
-  echo
-else
-    echo "Better luck on next login."
-fi
-fi
-fi
-fi
-fi
+myString="TestString"
+referenceString="password"
+
+[ $myString = $referenceString ] && echo "Correct!" || echo "Incorrect."
